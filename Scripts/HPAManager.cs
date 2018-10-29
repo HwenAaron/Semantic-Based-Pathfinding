7d14df8bcda8568680ad7cb5fb52fe8e2e66e81b
Haoran Wen <haoranwen1@gmail.com>
2018-10-29 08:32:19 -0400

    updated scripts

---
+++ Scripts/HPAManager.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPAManager : MonoBehaviour
+{
+
+
+    //references
+    public Map m;
+
+
+
+    // used for testing and other debugging
+    // Use this for initialization
+    void Start()
+    {
+        m.GetMapDimensions();
+        m.BuildCellGrid(.1f);
+        m.DefineZones();
+        m.FindThresholds();
+
+    }
+
+
+}
